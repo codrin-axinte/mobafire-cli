10df1164a9470141bf2e49858883d9a2c9e6f072
Codrin Axinte <codrin.axinte@outlook.com>
2020-12-09 04:56:01 +0100

    wip

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mobafire CLI
+
+A CLI for [mobafire.com](https://mobafire.com) website built using python.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mobafire-cli',
+    version='0.0.1',
+    packages=['src', 'src.parsers'],
+    url='https://github.com/codrin-axinte/mobafire-cli',
+    license='MIT',
+    author='Codrin Axinte',
+    author_email='xntcodrin@yahoo.com',
+    description='A CLI for mobafire.com website built using python.'
+)
